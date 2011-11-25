4fda2d8ae7b38b3bdab49abba06641c55406f619
Kyle Gordon <kyle@lodge.glasgownet.com>
2011-11-25 23:59:26 +0000

    More updates to it
    git-svn-id: svn+ssh://lodge.glasgownet.com/home/bagpuss/svn/home/scripts/cartracker@2381 f8e7472a-231e-0410-bb27-8899297d3c25

--- install.sh
+++ install.sh
@@ -3,15 +3,26 @@
 
 LCDPROCSRC=/usr/src/lcdproc/
 CARTRACKERSRC=/usr/src/cartracker/
+LCDPROCETC=/etc/lcdproc/
+
+mkdir $LCDPROCETC
 
 cd $LCDPROCSRC
 ./configure --enable-drivers=SureElec --prefix=/usr
 make install
 
 cp $LCDPROCSRC/scripts/debian/lcdproc.init /etc/init.d/lcdproc
-cp CARTRACKERSRC/LCD
+cp $LCDPROCSRC/scripts/debian/lcdproc.LCDd.init /etc/init.d/LCDd
+cp $CARTRACKERSRC/LCDd.conf $LCDPROCETC/
+cp $LCDPROCSRC/scripts/debian/lcdproc.LCDd.default /etc/default/LCDd
 chmod +x /etc/init.d/lcdproc
+chmod +x /etc/init.d/LCDd
 update-rc.d lcdproc defaults
+update-rc.d LCDd defaults
+
+## Enable LCDd
+sed -i -e s/START=no/START=yes/ /etc/default/LCDd
+sed -i -e s/#CONFIGFILE/CONFIGFILE/ /etc/default/LCDd
 
 cd $CARTRACKERSRC
 
