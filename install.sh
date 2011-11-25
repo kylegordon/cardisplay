b95a1317c9fb100b923dda364d767a777172a555
Kyle Gordon <kyle@lodge.glasgownet.com>
2011-11-25 23:41:27 +0000

    Renamed lcdproc config file
    git-svn-id: svn+ssh://lodge.glasgownet.com/home/bagpuss/svn/home/scripts/cartracker@2379 f8e7472a-231e-0410-bb27-8899297d3c25

--- install.sh
+++ install.sh
@@ -2,13 +2,16 @@
 ## Copies stuff to the right places
 
 LCDPROCSRC=/usr/src/lcdproc/
+CARTRACKERSRC=/usr/local/cartracker/
 
 cd $LCDPROCSRC
 ./configure --enable-drivers=SureElec --prefix=/usr
 make install
 
 cp $LCDPROCSRC/scripts/debian/lcdproc.init /etc/init.d/lcdproc
+cp CARTRACKERSRC/LCD
 chmod +x /etc/init.d/lcdproc
 update-rc.d lcdproc defaults
 
+cd $CARTRACKERSRC
 
