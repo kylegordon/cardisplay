93c2c9ea0db78dcfc5c10e03912b2fb7bec5894c
Kyle Gordon <kyle@lodge.glasgownet.com>
2011-11-27 17:26:19 +0000

    Don't want lcdproc client running (it displays systems stats, etc, and overloads the USB1 bus)
    git-svn-id: svn+ssh://lodge.glasgownet.com/home/bagpuss/svn/home/scripts/cartracker@2383 f8e7472a-231e-0410-bb27-8899297d3c25

--- install.sh
+++ install.sh
@@ -11,18 +11,16 @@ cd $LCDPROCSRC
 ./configure --enable-drivers=SureElec --prefix=/usr
 make install
 
-cp $LCDPROCSRC/scripts/debian/lcdproc.init /etc/init.d/lcdproc
 cp $LCDPROCSRC/scripts/debian/lcdproc.LCDd.init /etc/init.d/LCDd
 cp $CARTRACKERSRC/LCDd.conf $LCDPROCETC/
 cp $LCDPROCSRC/scripts/debian/lcdproc.LCDd.default /etc/default/LCDd
-chmod +x /etc/init.d/lcdproc
 chmod +x /etc/init.d/LCDd
-update-rc.d lcdproc defaults
 update-rc.d LCDd defaults
 
 ## Enable LCDd
 sed -i -e s/START=no/START=yes/ /etc/default/LCDd
 sed -i -e s/#CONFIGFILE/CONFIGFILE/ /etc/default/LCDd
+## Do something to update DriverPath in LCDd.conf
 
 cd $CARTRACKERSRC
 
